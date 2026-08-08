a10b2df913496928cd1d11993cc19c81be266eed
blackST4Rez <maharjanraka2015@gmail.com>
2026-08-08 11:10:15 +0545

    Some Minor UI Changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import Portfo from '/Portfolio.svg'
 import Attendance from '/Attendance.svg'
 import Guitar from '/Guitar.svg'
 import Movie from '/Movie.svg'
-import { Css3Original, ExpressOriginal, GithubOriginal, GitOriginal, Html5Original, JavascriptOriginal, MongodbOriginal, NodejsOriginal, ReactOriginal, TailwindcssOriginal } from 'devicons-react'
+import { Css3Original, ExpressOriginal, GithubOriginal, GitOriginal, Html5Original, JavascriptOriginal, MongodbOriginal, NodejsOriginal, ReactOriginal, ReduxOriginal, TailwindcssOriginal } from 'devicons-react'
 import { BiLogoGithub } from 'react-icons/bi'
 
 const Home = () => {
@@ -25,7 +25,8 @@ const Home = () => {
                         <span><Css3Original size={50}/></span>
                         <span><JavascriptOriginal size={50}/></span>
                         <span><ReactOriginal size={50}/></span>
-                        <span><TailwindcssOriginal size={50}/></span>
+                        <span><TailwindcssOriginal size={50} /></span>
+                        <span><ReduxOriginal size={50}/></span>
                         <span><NodejsOriginal size={50}/></span>
                         <span><ExpressOriginal size={50}/></span>
                         <span><MongodbOriginal size={50}/></span>
@@ -42,7 +43,7 @@ const Home = () => {
                     <div className="flex flex-col lg:flex-row lg:gap-20">
 
                     <ul>
-                        <li className='relative flex flex-col gap-1 border-l p-6 before:absolute before:-left-1.5 before:top-1/2 before:h-3 before:w-3 before:-translate-y-1/2 before:rounded-full before:bg-black dark:before:bg-white before:transform'>
+                        <li className= 'relative flex flex-col gap-1 border-l p-6 before:absolute before:-left-1.5 before:top-1/2 before:h-3 before:w-3 before:-translate-y-1/2 before:rounded-full before:bg-black dark:before:bg-white before:transform'>
                             <span className='text-lg font-semibold'>A Portfolio</span>
                             <img className='w-80 h-80' src={Portfo} alt="Portfolio-img" />
                             <span className='text-sm text-blue-600'>Tech Stack: React & TailwindCSS</span>
@@ -71,7 +72,7 @@ const Home = () => {
                                     Live Demo
                                     <ArrowBigRightIcon className="h-4 w-4 -rotate-45" />
                                 </a>
-                                <a href="https://github.com/blackST4Rez/Portfolio" target="_blank" rel="noreferrer" className="w-max mt-4 inline-flex items-center gap-2 border border-black px-4 py-2 text-sm font-semibold transition-all duration-500 bg-black text-white hover:bg-blue-700 hover:text-white dark:border-black dark:bg-blue-700 dark:text-white dark:hover:bg-black dark:hover:text-white dark:hover:border-white">
+                                <a href="https://github.com" target="_blank" rel="noreferrer" className="w-max mt-4 inline-flex items-center gap-2 border border-black px-4 py-2 text-sm font-semibold transition-all duration-500 bg-black text-white hover:bg-blue-700 hover:text-white dark:border-black dark:bg-blue-700 dark:text-white dark:hover:bg-black dark:hover:text-white dark:hover:border-white">
                                     Github
                                     <BiLogoGithub className="h-4 w-4" />
                                 </a>
@@ -81,8 +82,8 @@ const Home = () => {
                     <ul> 
                         <li className='relative flex flex-col gap-1 border-l p-6 before:absolute before:-left-1.5 before:top-1/2 before:h-3 before:w-3 before:-translate-y-1/2 before:rounded-full before:bg-black dark:before:bg-white before:transform'>
                             <span className='text-lg font-semibold'>E-Commerce Website</span>
-                            <img className='w-80 h-80' src={Guitar} alt="Attendance-img" />
-                            <span className='text-sm text-blue-600'>Tech Stack: MongoDB, Express, React & NodeJS</span>
+                            <img className='w-75 h-75' src={Guitar} alt="Attendance-img" />
+                            <span className='text-sm text-blue-600'>Tech Stack: MongoDB, Express, React & NodeJS with Khalti Payment Integration</span>
                             <p className='text-sm text-gray-600 dark:text-gray-400'>A musical instrument e-commerce website for the upcoming Superstar's.</p>
                             <div className="flex gap-4">
                                 <a href="https://example.com" target="_blank" rel="noreferrer" className="w-max mt-4 inline-flex items-center gap-2 border border-black px-4 py-2 text-sm font-semibold transition-all duration-500 bg-black text-white hover:bg-blue-700 hover:text-white dark:border-black dark:bg-blue-700 dark:text-white dark:hover:bg-black dark:hover:text-white dark:hover:border-white">
