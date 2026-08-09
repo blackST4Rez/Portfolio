756de8be0fe00b721a5348bdf06f8aba761a3c2d
blackST4Rez <maharjanraka2015@gmail.com>
2026-08-09 07:04:31 +0545

    added one more project on the li.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
-import { ArrowBigRightIcon } from 'lucide-react'   
+import { ArrowBigRightIcon } from 'lucide-react'
+import GithubSearcher from '/GithubSearcher.svg'   
 import Portfo from '/Portfolio.svg'
 import Attendance from '/Attendance.svg'
 import Guitar from '/Guitar.svg'
@@ -28,7 +29,7 @@ const Home = () => {
                         <span><TailwindcssOriginal size={50} /></span>
                         <span><ReduxOriginal size={50}/></span>
                         <span><NodejsOriginal size={50}/></span>
-                        <span><ExpressOriginal size={50}/></span>
+                        <span><ExpressOriginal size={50} className='text-gray-500' /></span>
                         <span><MongodbOriginal size={50}/></span>
                         <span><GitOriginal size={50}/></span>
                         <span><GithubOriginal size={50}/></span>
@@ -40,17 +41,17 @@ const Home = () => {
                     <h1 className='text-2xl font-bold
                     md:text-4xl'>Projects</h1>
 
-                    <div className="flex flex-col lg:flex-row lg:gap-20">
+                    <div className="flex flex-col lg:flex-row lg:gap-30">
 
                     <ul>
                         <li className= 'relative flex flex-col gap-1 border-l p-6 before:absolute before:-left-1.5 before:top-1/2 before:h-3 before:w-3 before:-translate-y-1/2 before:rounded-full before:bg-black dark:before:bg-white before:transform'>
                             <span className='text-lg font-semibold'>A Portfolio</span>
-                            <img className='w-80 h-80' src={Portfo} alt="Portfolio-img" />
+                            <img className='w-90 h-90' src={Portfo} alt="Portfolio-img" />
                             <span className='text-sm text-blue-600'>Tech Stack: React & TailwindCSS</span>
                             <p className='text-sm text-gray-600
                             dark:text-gray-400'>Simple, rlegant & responsive portfolio for personal use. </p>
                             <div className="flex gap-4">
-                                <a href="https://example.com" target="_blank" rel="noreferrer" className="w-max mt-4 inline-flex items-center gap-2 border border-black px-4 py-2 text-sm font-semibold transition-all duration-500 bg-black text-white hover:bg-blue-700 hover:text-white dark:border-black dark:bg-blue-700 dark:text-white dark:hover:bg-black dark:hover:text-white dark:hover:border-white">
+                                <a href="https://portfolio-e42g.onrender.com/" target="_blank" rel="noreferrer" className="w-max mt-4 inline-flex items-center gap-2 border border-black px-4 py-2 text-sm font-semibold transition-all duration-500 bg-black text-white hover:bg-blue-700 hover:text-white dark:border-black dark:bg-blue-700 dark:text-white dark:hover:bg-black dark:hover:text-white dark:hover:border-white">
                                     Live Demo
                                     <ArrowBigRightIcon className="h-4 w-4 -rotate-45" />
                                 </a>
@@ -112,7 +113,26 @@ const Home = () => {
                                 </a>
                             </div>
                         </li>
-                    </ul>                        
+                        </ul>
+
+                    <ul> 
+                        <li className='relative flex flex-col gap-1 border-l p-6 before:absolute before:-left-1.5 before:top-1/2 before:h-3 before:w-3 before:-translate-y-1/2 before:rounded-full before:bg-black dark:before:bg-white before:transform'>
+                            <span className='text-lg font-semibold'>Github User Searcher</span>
+                            <img className='w-75 h-75' src={GithubSearcher} alt="Attendance-img" />
+                            <span className='text-sm text-blue-600'>Tech Stack: React & TailwindCSS</span>
+                            <p className='text-sm text-gray-600 dark:text-gray-400'>Github User Finder using React & TailwindCSS.</p>
+                            <div className="flex gap-4">
+                                <a href="https://githubuserfinder-mp7e.onrender.com/" target="_blank" rel="noreferrer" className="w-max mt-4 inline-flex items-center gap-2 border border-black px-4 py-2 text-sm font-semibold transition-all duration-500 bg-black text-white hover:bg-blue-700 hover:text-white dark:border-black dark:bg-blue-700 dark:text-white dark:hover:bg-black dark:hover:text-white dark:hover:border-white">
+                                    Live Demo
+                                    <ArrowBigRightIcon className="h-4 w-4 -rotate-45" />
+                                </a>
+                                <a href="https://github.com" target="_blank" rel="noreferrer" className="w-max mt-4 inline-flex items-center gap-2 border border-black px-4 py-2 text-sm font-semibold transition-all duration-500 bg-black text-white hover:bg-blue-700 hover:text-white dark:border-black dark:bg-blue-700 dark:text-white dark:hover:bg-black dark:hover:text-white dark:hover:border-white">
+                                    Github
+                                    <BiLogoGithub className="h-4 w-4" />
+                                </a>
+                            </div>
+                        </li>
+                    </ul>      
 
                     </div>
                 </div>
